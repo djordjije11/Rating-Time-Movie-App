0ab737044209d32e56a1271b2c2034521993a092
jelena <repac01jelena@gmail.com>
2023-03-28 15:38:59 +0200

    Added NavBar component -  Initial appearance

---
+++ RatingTimeFrontEnd/rating-time-webapp/src/components/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter} from "react-router-dom";
+import NavBar from "./NavBar";
+//import './App.css';
+
+function App() {
+  return (
+   <BrowserRouter>
+    <NavBar />
+
+   </BrowserRouter>
+  );
+}
+
+export default App;
