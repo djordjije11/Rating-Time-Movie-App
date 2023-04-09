6e0659d0cf0b1dfe2d23d697312c0b90a214500d
djordjije11 <djordjijeradovic24@gmail.com>
2023-04-09 11:55:38 +0200

    Lowercased api routes

--- RatingTimeBackEnd/RatingTime.API/Program.cs
+++ RatingTimeBackEnd/RatingTime.API/Program.cs
@@ -36,6 +36,8 @@ builder.Services.AddAutoMapper(typeof(UserLogin).Assembly);
 builder.Services.AddControllers()
                 .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
 
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
