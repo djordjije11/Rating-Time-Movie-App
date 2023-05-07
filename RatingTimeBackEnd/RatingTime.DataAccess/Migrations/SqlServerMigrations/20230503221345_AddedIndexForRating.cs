a7e908677f645e431e7b2655de1af09d2a19eb24
djordjije11 <djordjijeradovic24@gmail.com>
2023-05-07 13:36:53 +0200

    bad commit

---
+++ RatingTimeBackEnd/RatingTime.DataAccess/Migrations/SqlServerMigrations/20230503221345_AddedIndexForRating.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RatingTime.DataAccess.Migrations.SqlServerMigrations
+{
+    /// <inheritdoc />
+    public partial class AddedIndexForRating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_rating_user_id",
+                table: "rating");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_rating_user_id_movie_id",
+                table: "rating",
+                columns: new[] { "user_id", "movie_id" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_rating_user_id_movie_id",
+                table: "rating");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_rating_user_id",
+                table: "rating",
+                column: "user_id");
+        }
+    }
+}
